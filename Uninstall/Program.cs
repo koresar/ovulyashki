ead630293cfdae54a4482fa4cf9555d1357fc71c
ks <ks@9bac9d31-76d4-0d47-98b6-109c82c62334>
2009-02-10 23:50:42 +0000

    Uninstaller added.

---
+++ Uninstall/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Uninstall
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            try
+            {
+                if (MessageBox.Show("Уверена, что хочешь удалить Овуляшки?", "Подумай еще раз",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                {
+                    Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.System) + @"\msiexec.exe", 
+                        @" /x {334556E0-10B1-4308-A487-DA137AF1BCF1}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Что-то ужасное произошло! Попробуй удалить Овуляшки через Установку/Удаление программ.\n\n" + 
+                    ex.Message, "ОЙ!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
