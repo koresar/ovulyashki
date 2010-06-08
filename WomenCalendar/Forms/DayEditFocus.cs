717ce246ae3369972d8f879d05bfa7e2b0154a10
kore_sar <kore_sar@9bac9d31-76d4-0d47-98b6-109c82c62334>
2010-06-08 18:01:54 +0000

    Commit forgotten file.

---
+++ WomenCalendar/Forms/DayEditFocus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WomenCalendar
+{
+    /// <summary>
+    /// The place on the day edit window. This enum used to identify focus position.
+    /// </summary>
+    public enum DayEditFocus
+    {
+        /// <summary>
+        /// The note edit text box.
+        /// </summary>
+        Note, 
+
+        /// <summary>
+        /// The Basal Body Temperature edit box.
+        /// </summary>
+        BBT, 
+        
+        /// <summary>
+        /// The length of blooding numeric edit box.
+        /// </summary>
+        Length, 
+
+        /// <summary>
+        /// The schdules.
+        /// </summary>
+        Schedules
+    };
+}
